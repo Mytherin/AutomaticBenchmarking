53b5b5982ca3b0d7005d35d33484de54d95f3be2
Mytherin <mark.raasveldt@gmail.com>
2016-06-12 14:45:57 +0200

    Tabs/spaces.

--- tpch.py
+++ tpch.py
@@ -7,10 +7,10 @@ scalefactor = 1
 def generate(basedir):
     os.chdir(basedir)
     tpchpath = os.path.join(basedir, 'TPCH')
-	dbgenpath = os.path.join(tpchpath, 'tpch', 'dbgen')
+    dbgenpath = os.path.join(tpchpath, 'tpch', 'dbgen')
     if not os.path.exists(tpchpath):
         os.system('hg clone ssh://hg@dev.monetdb.org/benchmarks/ %s' % tpchpath)
         os.rename(os.path.join(dbgenpath, 'makefile.suite'), os.path.join(dbgenpath, 'Makefile'))
         os.chdir(dbgenpath)
         os.system('make')
-	os.system('%s -s %d' % (os.path.join(dbgenpath, 'dbgen'), scalefactor))
+    os.system('%s -s %d' % (os.path.join(dbgenpath, 'dbgen'), scalefactor))
